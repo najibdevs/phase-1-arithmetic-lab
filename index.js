5104d1ed7c068072cdba173de4bb705d8fe6f0fd
najibdevs <najibsoftwareeng@gmail.com>
2024-03-20 10:23:56 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 20
+let num2 = 3.1
+let num3 = 13
+let num4 = 9
 
-// Write your code here
+let multiply = num2 * num1
+
+let random = Math.floor(Math.random() * 100) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(num1, num2, num3, num4)
